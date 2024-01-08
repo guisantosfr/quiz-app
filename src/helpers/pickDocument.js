dbb5e586c2f8647536d254dd91accd5b007e4be4
Francisco Guilherme <santosgui678@gmail.com>
2024-01-08 11:48:12 -0300

    Get data from worksheet

--- src/helpers/pickDocument.js
+++ src/helpers/pickDocument.js
@@ -1,18 +1,44 @@
-import * as DocumentPicker from 'expo-document-picker';
+import { getDocumentAsync } from 'expo-document-picker';
+import { readAsStringAsync } from 'expo-file-system';
+import { read, utils } from 'xlsx';
 
 export const pickDocumentAsync = async () => {
-  let result = await DocumentPicker.getDocumentAsync({
+  let result = await getDocumentAsync({
     //.xlsx
     type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
   });
 
   /*
-  {"assets": [{"mimeType": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "name": "Turma.xlsx", "size": 9231, "uri": "file:///data/user/0/host.exp.exponent/cache/DocumentPicker/382845a1-ab1b-4f81-809e-4865a3d9e92a.xlsx"}], "canceled": false}
+  {"assets":
+    [
+      {
+      "mimeType": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      "name": "Turma.xlsx",
+      "size": 9231,
+      "uri": "file:///data/user/0/host.exp.exponent/cache/DocumentPicker/382845a1-ab1b-4f81-809e-4865a3d9e92a.xlsx"
+    }
+  ],
+      "canceled": false
+    }
   */
 
   if (!result.canceled) {
-    console.log(result);
-  } else {
-    console.log("You didnt select any document");
+    const temp = await readAsStringAsync(result.assets[0].uri, {
+      encoding: 'base64'
+    });
+
+    const workbook = read(temp, { type: 'base64' });
+
+    const wsName = workbook.SheetNames[0];
+    const ws = workbook.Sheets[wsName];
+
+    //Array de objetos
+    const data = utils.sheet_to_json(ws);
+
+    //Array de arrays
+    //const data = utils.sheet_to_json(ws, { header: 1 });
+
+    console.log(data);
   }
+
 }
