3abb5a0d0ec95592c9d12f9996f9f2c7803aba0f
Guilherme Santos <santosgui678@gmail.com>
2024-06-03 10:21:14 -0300

    Prepare quiz to solve

--- src/screens/SelectQuizScreen.jsx
+++ src/screens/SelectQuizScreen.jsx
@@ -1,7 +1,41 @@
-import { Text } from "react-native";
+import { ActivityIndicator, Dimensions, FlatList, SafeAreaView, Text } from "react-native";
+
+import globalStyles from '../utils/globalStyles';
+import useQuizzes from "../hooks/useQuizzes";
+import theme from "../theme";
+import Card from "../components/Card";
 
 export default function SelectQuizScreen() {
+  const { quizzes } = useQuizzes();
+
+  const renderItem = ({ item }) => (
+    //TODO
+    <Card onPress={() => navigation.navigate('Quiz Detail', { id: item._id})}>
+      <Text>{item.name}</Text>
+    </Card>
+  )
+
   return (
-      <Text>Selecione um questionário</Text>
+      <SafeAreaView style={globalStyles.container}>
+        {
+          !quizzes ?
+            <ActivityIndicator size="large" color={theme.colors.lightBlue} />
+            :
+            (
+              quizzes.length === 0 ?
+                <Text style={globalStyles.text}>Não há questionários cadastrados</Text>
+                :
+                <>
+                  <Text style={[globalStyles.text, globalStyles.title]}>Selecione um questionário</Text>
+                  <FlatList
+                    data={quizzes}
+                    keyExtractor={item => item._id}
+                    renderItem={renderItem}
+                    style={{width: Dimensions.get('window').width * 0.9}}
+                  />
+                </>
+            )
+        }
+      </SafeAreaView>
   );
 }
