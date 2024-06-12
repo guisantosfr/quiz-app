fdb5f75ca91bb238d9cce9272daa0984ce907cb3
Guilherme Santos <santosgui678@gmail.com>
2024-06-12 19:30:20 -0300

    Include time and logic to answer

--- src/screens/SolveQuizScreen.jsx
+++ src/screens/SolveQuizScreen.jsx
@@ -14,12 +14,37 @@ export default function SolveQuizScreen({ navigation }) {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [selectedAnswer, setSelectedAnswer] = useState(null);
 
+    const [timer, setTimer] = useState(10);
+    const [timeIsOver, setTimeIsOver] = useState(false);
+
     useEffect(() => {
         setShuffledQuestions(shuffleArray(quiz.questions));
     }, []);
 
+    useEffect(() => {
+        let interval = null;
+
+        if (!timeIsOver && timer > 0) {
+          interval = setInterval(() => {
+            setTimer((prevTimer) => prevTimer - 1);
+          }, 1000);
+        } else if (timer === 0) {
+          setTimeIsOver(true);
+        }
+
+        return () => clearInterval(interval);
+      }, [timer, timeIsOver, currentQuestionIndex]);
+
     const handleNextQuestion = () => {
         setCurrentQuestionIndex((prevIndex) => Math.min(prevIndex + 1, shuffledQuestions.length - 1));
+
+        setTimer(10);
+        setTimeIsOver(false);
+        setSelectedAnswer(null);
+
+        //navigate
+        //compute answer
+        //refresh states
     };
     
     return (
@@ -31,6 +56,13 @@ export default function SolveQuizScreen({ navigation }) {
                 (
                     <>
                     <Text style={globalStyles.heading}>{quiz.name}</Text>
+                    {
+                        timeIsOver ?
+                        <Text style={globalStyles.subheading}>Tempo esgotado</Text>
+                        :
+                        <Text style={globalStyles.subheading}>Tempo:  {timer}</Text>
+
+                    }
                     <Text style={globalStyles.subheading}>Questão {currentQuestionIndex + 1} de {quiz.questions.length}</Text>
                     <Text style={globalStyles.subheading}>Assunto: {shuffledQuestions[currentQuestionIndex].topic}</Text>
                     <Text style={styles.question}>{shuffledQuestions[currentQuestionIndex].question}</Text>
@@ -39,6 +71,7 @@ export default function SolveQuizScreen({ navigation }) {
                     <TouchableOpacity
                     style={[styles.button, styles.true, selectedAnswer === true && styles.selectedButton]}
                     onPress={() => setSelectedAnswer(true)}
+                    disabled={timeIsOver}
                     >
                         <Text style={styles.buttonText}>Verdadeiro</Text>
                     </TouchableOpacity>
@@ -46,22 +79,27 @@ export default function SolveQuizScreen({ navigation }) {
                     <TouchableOpacity
                     style={[styles.button, styles.false, selectedAnswer === false && styles.selectedButton]}
                     onPress={() => setSelectedAnswer(false)}
+                    disabled={timeIsOver}
                     >
                         <Text style={styles.buttonText}>Falso</Text>
                     </TouchableOpacity>
                     </View>
 
                     {selectedAnswer !== null && (
-                        <Text style={styles.answerText}>
+                        <Text style={[globalStyles.subheading, styles.answerText]}>
                             Você selecionou: {selectedAnswer ? "Verdadeiro" : "Falso"}
                         </Text>
                     )}
- 
+
+                    
                     {
-                        currentQuestionIndex === shuffledQuestions.length - 1 ?
+                        timeIsOver &&
+                        (
+                            currentQuestionIndex === shuffledQuestions.length - 1 ?
                             <Button text="Finalizar" onPress={() => {}}/>
                             :
                             <Button text="Ver resposta" onPress={handleNextQuestion}/>
+                        )
                     }
                     
                     </>
@@ -109,7 +147,8 @@ const styles = StyleSheet.create({
     buttonText:{
         textAlign: 'center',
         fontWeight: 'bold',
-        fontSize: theme.fontSizes.medium
+        fontSize: theme.fontSizes.medium,
+        color: theme.colors.white
     },
 
 })
